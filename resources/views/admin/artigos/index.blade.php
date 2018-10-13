541349239f7f7151e069ef97b4cf579a3e397679
Roberto Ribeiro <robertov82008@gmail.com>
2018-10-13 20:11:57 -0300

    1.12 - Tabela de Lista de Artigos

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -3,7 +3,34 @@
 @section('content')
     <pagina tamanho="12">
         <painel titulo="Lista de Artigos">
-            Teste de conteúdo
+            <a href="#">Criar</a>
+
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Título</th>
+                        <th>Descrição</th>
+                        <th>Autor</th>
+                        <th>Data</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Título</td>
+                        <td>Descrição</td>
+                        <td>Autor</td>
+                        <td>Data</td>
+                        <td>
+                            <a href="#">Editar</a>
+                            <a href="#">Deletar</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </painel>
     </pagina>
 @endsection
